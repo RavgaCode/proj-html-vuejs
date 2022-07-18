3ce0d46c4c729bd97b2295370c32f2ae7804f4e0
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-07-19 01:05:40 +0200

    Services Section CSS

--- src/components/ServicesSection.vue
+++ src/components/ServicesSection.vue
@@ -3,20 +3,25 @@
     <div class="container">
       <!-- Services introduction -->
       <h5>OUR BUSINESS AREAS</h5>
-      <H3>Excellence in <span>Services</span></H3>
-      <div>
-        <p>
+      <H3>Excellence in <span class="highlighted_green">Services</span></H3>
+      <div class="d-flex justify-content-between">
+        <p class="intro_p">
           We are leaders in providing consultancy services with a set of
           cutting-edge technologies and a team of experienced and renowned
           professionals. These are some options that you can hire.
         </p>
-        <button>SEE ALL</button>
+        <div><a href="#" class="ms_btn btn_green">SEE ALL</a></div>
       </div>
       <!-- Services grid -->
       <div class="row">
         <div class="col-4 my-3">
           <div class="services_card">
-            <span>Icona</span>
+            <div class="d-flex justify-content-between green_icons">
+              <div><a href="#">Icona</a></div>
+              <div>
+                <a href="#"><i class="fa-solid fa-arrow-right"></i></a>
+              </div>
+            </div>
             <h4>Audit & Assurance</h4>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
           </div>
@@ -74,10 +79,26 @@ section {
   background-color: #eef4ed;
   color: #848486;
   height: 800px;
+  display: flex;
+  align-items: center;
+}
+h3,
+h4 {
+  color: black;
+}
+.intro_p {
+  width: 70%;
+}
+.ms_btn {
+  width: 90px;
 }
 .services_card {
   background-color: white;
   border-radius: 15px;
   padding: 2.5rem;
+}
+.green_icons a {
+  color: #058283;
+  text-decoration: none;
 }
 </style>
