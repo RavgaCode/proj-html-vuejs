27cc7b5ed22c19a0d21409f3ddfa488468b8b5cb
RavgaCode <roberto.giacomuzzo@gmail.com>
2022-07-19 01:27:37 +0200

    About Section CSS

--- src/components/AboutSection.vue
+++ src/components/AboutSection.vue
@@ -5,38 +5,43 @@
         <!-- Left Col - Text-->
         <div class="col-6 d-flex align-items-center">
           <div class="about_card">
-            <h5>ABOUT THE NETWORK</h5>
-            <h3><span>The</span> Company</h3>
+            <h5 class="my-3">ABOUT THE NETWORK</h5>
+            <h3 class="my-3">
+              <span class="highlighted_white">The</span> Company
+            </h3>
             <p>
               For 12 years we have been providing audit and warranty, financial
               advice, risk advice, taxes and related services to select clients.
             </p>
             <!-- About Info Cards -->
             <div class="row">
-              <div class="col-6">
+              <div class="col-6 my-3">
                 <h4><span>Logo </span>Tradition</h4>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
               </div>
-              <div class="col-6">
+              <div class="col-6 my-3">
                 <h4><span>Logo </span>Tradition</h4>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
               </div>
-              <div class="col-6">
+              <div class="col-6 my-3">
                 <h4><span>Logo </span>Tradition</h4>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
               </div>
-              <div class="col-6">
+              <div class="col-6 my-3">
                 <h4><span>Logo </span>Tradition</h4>
                 <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
               </div>
             </div>
             <!-- Buttons -->
             <div class="d-flex">
-              <button>GET IN TOUCH</button>
-              <button>READ MORE</button>
+              <div><a href="#" class="ms_btn btn_green">GET IN TOUCH</a></div>
+              <div>
+                <a href="#" class="ms_btn btn_transparent">READ MORE</a>
+              </div>
             </div>
           </div>
         </div>
+
         <!-- Right-Col - Img -->
         <div class="col-6">
           <img src="../assets/img/about-4.jpg" alt="About-img" />
@@ -54,11 +59,18 @@ export default {
 <style lang="scss" scoped>
 @import "../style/style.scss";
 section {
-  background-color: #000000;
+  background-color: #111117;
   color: white;
   height: 80vh;
 }
+p {
+  color: #b1bbcd;
+}
 img {
-  max-height: 80vh;
+  max-width: 80vh;
+}
+.ms_btn {
+  margin-right: 1rem;
+  margin-top: 2rem;
 }
 </style>
